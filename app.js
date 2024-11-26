f8a0af78fd1313c4a6eb53893680e1318e51f895
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-26 20:36:07 +0530

    up

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const heading=document.createElement("h1");
-heading.innerHTML="Hello world";
-const root= document.getElementById("root");
-root.appendChild(heading);
+
+//HELLO WORLD IN JS
+// const heading=document.createElement("h1");
+// heading.innerHTML="Hello world";
+// const root= document.getElementById("root");
+// root.appendChild(heading);
+
+//HELLO WORLD IN REACTJS
+const heading = React.createElement("h1", {id: "heading"}, "Hello World form React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
