2a926f5a00bb03ae9943225c4d5e94db5c4e0157
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-26 22:43:28 +0530

    up

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
 //HELLO WORLD IN JS
 // const heading=document.createElement("h1");
@@ -6,6 +8,6 @@
 // root.appendChild(heading);
 
 //HELLO WORLD IN REACTJS
-const heading = React.createElement("h1", {id: "heading"}, "Hello World form React!");
+const heading = React.createElement("h1", {id: "heading"}, "Hello World form React!!!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,13 @@
+//HELLO WORLD IN JS
+// const heading=document.createElement("h1");
+// heading.innerHTML="Hello world";
+// const root= document.getElementById("root");
+// root.appendChild(heading);
+//HELLO WORLD IN REACTJS
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World form React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
