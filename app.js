393d452400504df41847690e7e6090fcd17c47f3
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-27 21:17:44 +0530

    up

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";// Corrected import for React 18
 
-//HELLO WORLD IN JS
-// const heading=document.createElement("h1");
-// heading.innerHTML="Hello world";
-// const root= document.getElementById("root");
+// HELLO WORLD IN JS
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello world";
+// const root = document.getElementById("root");
 // root.appendChild(heading);
 
-//HELLO WORLD IN REACTJS
-const heading = React.createElement("h1", {id: "heading"}, "Hello World form React!!!!");
+// HELLO WORLD IN REACTJS
+ // Correct import for React 18
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from ReactJS!");
+
+// Use `createRoot` from `react-dom/client`
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(heading);
+
